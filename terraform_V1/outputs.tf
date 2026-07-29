cbfceff7443cd758db1f2bf15c4eb5b1f0eeb35e
Aquaphobique <romain.castel.hugues@gmail.com>
2026-07-29 16:54:17 +0200

    chore: retire terraform_V1/V2, scripts et ansible (hors périmètre TP1)

--- terraform_V1/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "romain_webserver_public_ip_address" {
-  description = "Adresse IP publique de l'instance EC2 créée"
-  value       = aws_instance.romain_webserver.public_ip
-}
--- terraform_V2/modules/compute/main.tf
+++
@@ -1,9 +0,0 @@
-data "aws_ami" "ubuntu" {
-    filter {
-        name = "name"
-        values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
-    }
-    tags = {}
-    owners = ["099720109477"] # Canonical
-    most_recent = true
-}
